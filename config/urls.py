76aa05230edcc73097926783a2d0be8023c7bddb
Khadizh <hadyousha6@gmail.com>
2026-01-01 22:58:07 +0600

    Swagger Documentation

--- config/urls.py
+++ config/urls.py
@@ -14,17 +14,59 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Online Courses API",
+        default_version='v1',
+        description="""
+        API для платформы онлайн-курсов
+
+        ## Основные возможности:
+        - Просмотр курсов, модулей и уроков
+        - Регистрация и авторизация пользователей
+        - Покупка курсов
+        - Отслеживание прогресса обучения
+        - Получение сертификатов
+        - Отзывы и поддержка
+
+        ## Аутентификация:
+        Используется Session Authentication. 
+        Для тестирования войдите через /api-auth/login/
+        """,
+        terms_of_service="https://www.skillsup.kg/terms/",
+        contact=openapi.Contact(email="support@skilssup.kg"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('courses.urls')),
+
+
     path('api-auth/', include('rest_framework.urls')),
+
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger.yaml', schema_view.without_ui(cache_timeout=0), name='schema-yaml'),
+
+
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
