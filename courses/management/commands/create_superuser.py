c36f8143ac3c1b18976c0ed4c1fc2b736a88fdc6
Khadizh <hadyousha6@gmail.com>
2026-01-07 21:18:56 +0600

    создание супер пользователя

--- courses/management/commands/create_superuser.py
+++ courses/management/commands/create_superuser.py
@@ -6,16 +6,18 @@ User = get_user_model()
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "luntik")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "luntik2703")
 
-        if not username or not password:
-            self.stdout.write("Superuser env vars not set")
-            return
+        user = User.objects.filter(username=username).first()
 
-        if User.objects.filter(username=username).exists():
-            self.stdout.write("Superuser already exists")
+        if user:
+            user.is_staff = True
+            user.is_superuser = True
+            user.set_password(password)
+            user.save()
+            self.stdout.write("Superuser UPDATED")
             return
 
         User.objects.create_superuser(
@@ -23,4 +25,4 @@ class Command(BaseCommand):
             email=email,
             password=password
         )
-        self.stdout.write("Superuser created")
+        self.stdout.write("Superuser CREATED")
